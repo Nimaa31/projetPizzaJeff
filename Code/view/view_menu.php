22cffb625eb53dcdc9720a05481a38f096b1f2ae
Amin-Eddine <amin.eddine31@icloud.com>
2022-05-13 11:57:55 +0200

    amin

--- Code/view/view_menu.php
+++ Code/view/view_menu.php
@@ -1,11 +1,5 @@
-<header>
-    <nav>
-        <li><a href=""></a> Accueil</li>
-        <li><a href=""></a> La Carte</li>
-        <li><a href=""></a> Blog</li>
-        <li><a href=""></a> Contact</li>
-        <li><a href=""></a> Réservation</li>
-        <li><a href=""></a> Panier</li>
-        <li><a href=""></a> Inscription</li>
-    </nav>
-</header>
+<?php
+include 'view_menu_connecte.php';
+include 'view_footer.html';
+?> 
+
