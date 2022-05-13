88bf0083471c80aac283f5664e14a46297a02915
Talique <ropars.k@gmail.com>
2022-05-13 10:31:48 +0200

    ctrl_acceuil

--- Code/view/view_menu_connecte.php
+++ Code/view/view_menu_connecte.php
@@ -41,11 +41,3 @@
       </ul>
     </div>
 </header>
-
-<footer>
-    <ul><a href=""><img src="../asset/image/instagrame2.png"></a> </ul>
-    <ul><a href=""><img src="../asset/image/facebook.png"></a> </ul>
-    <ul><a href=""><img src="../asset/image/reddit2.png"></a> </ul>
-</footer>
-</body>
-</html>
