2ab547fec7a3f1c4292f2a2dcd2ea41d881dc789
black <madimad@gmail.com>
2022-05-16 15:31:09 +0200

    heder update

--- Code/controler/controler_inscription.php
+++ Code/controler/controler_inscription.php
@@ -1,5 +1,5 @@
 <?php
-include '../view/view_menu_connecte.php';
+include '../view/view_menu_connecte.html';
 include '../view/view_inscription.php';
 include '../view/view_footer.php';
 
--- Code/controler/controler_update_compte.php
+++ Code/controler/controler_update_compte.php
@@ -1,5 +1,5 @@
 <?php
-include '../view/view_menu_connecte.php';
+include '../view/view_menu_connecte.html';
 include '../view/view_update_compte.php';
 include '../view/view_footer.php';
 
