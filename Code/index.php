e1e53b58bf3680af8cf05cc0a9693795fbee878f
Amin-Eddine <amin.eddine31@icloud.com>
2022-05-16 16:54:37 +0200

    test

--- Code/index.php
+++ Code/index.php
@@ -10,47 +10,47 @@
     ------------------------------------------------*/
     //test de la valeur $path dans l'URL et import de la ressource
     switch($path){
-        //route /code/ajouter_un_article -> ./controler/controler_add_article.php
-        case $path === "/code/accueil" : 
+        //route /amin/projetJeff/Code/ajouter_un_article -> ./controler/controler_add_article.php
+        case $path === "/amin/projetJeff/Code/accueil" : 
             include './controler/controler_accueil.php';
                 break ;
-        //route /code/ajouter_un_article -> ./controler/controler_add_article.php
-        case $path === "/code/ajouter_un_article" : 
+        //route /amin/projetJeff/Code/ajouter_un_article -> ./controler/controler_add_article.php
+        case $path === "/amin/projetJeff/Code/ajouter_un_article" : 
             include './controler/controler_add_article.php';
                 break ;
-        //route /code/ajouter_un_utilisateur -> ./controler/controler_add_user.php
-        case $path === "/code/ajouter_un_utilisateur" : 
+        //route /amin/projetJeff/Code/ajouter_un_utilisateur -> ./controler/controler_add_user.php
+        case $path === "/amin/projetJeff/Code/ajouter_un_utilisateur" : 
             include './controler/controler_utilisateur.php';
                 break ;
-        //route /code/connection -> ./controler/controler_connexion.php
-        case $path === "/code/connection" : 
+        //route /amin/projetJeff/Code/connection -> ./controler/controler_connexion.php
+        case $path === "/amin/projetJeff/Code/connection" : 
             include './controler/controler_connexion.php';
                 break ;
-        //route /code/deconnection -> ./controler/controler_deconnexion.php
-        case $path === "/code/deconnection" : 
+        //route /amin/projetJeff/Code/deconnection -> ./controler/controler_deconnexion.php
+        case $path === "/amin/projetJeff/Code/deconnection" : 
             include './controler/controler_deconnexion.php';
                 break ;
-        //route /code/supprimer_article -> ./controler/controler_delete_article.php
-        case $path === "/code/supprimer_article" : 
+        //route /amin/projetJeff/Code/supprimer_article -> ./controler/controler_delete_article.php
+        case $path === "/amin/projetJeff/Code/supprimer_article" : 
             include './controler/controler_delete_article.php';
                 break ;
-        //route /code/modifier_article -> ./controler/controler_modify_article.php
-        case $path === "/code/modifier_article" : 
+        //route /amin/projetJeff/Code/modifier_article -> ./controler/controler_modify_article.php
+        case $path === "/amin/projetJeff/Code/modifier_article" : 
             include './controler/controler_modify_article.php';
                 break ;
-        //route /code/menu -> ./controler/controler_catalogue.php
-        case $path === "/code/menu" : 
+        //route /amin/projetJeff/Code/menu -> ./controler/controler_catalogue.php
+        case $path === "/amin/projetJeff/Code/menu" : 
             include './controler/controler_catalogue.php';
                 break ;
-        //route /code/blog -> ./controler/controler_show_all_article.php
-        case $path === "/code/blog" : 
+        //route /amin/projetJeff/Code/blog -> ./controler/controler_show_all_article.php
+        case $path === "/amin/projetJeff/Code/blog" : 
             include './controler/controler_show_all_article.php';
                 break ;
-        case $path === "/code/afficher_les_produits" : 
+        case $path === "/amin/projetJeff/Code/afficher_les_produits" : 
             include './controler/controler_show_all_produit.php';
                 break ;
         //route en cas d'erreur
-        case $path === "/code/erreur" : 
+        case $path === "/amin/projetJeff/Code/erreur" : 
             include './controler/error.php';
                 break ;
     }
