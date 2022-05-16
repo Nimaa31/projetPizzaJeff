542e33b558d42eb89a175856d49c7cf5bb485122
talique <ropars.k@gmail.com>
2022-05-16 10:49:57 +0200

    ok

--- Code/controler/controler_show_all_produit.php
+++ Code/controler/controler_show_all_produit.php
@@ -6,10 +6,12 @@
     include './utils/connectBdd.php';
     //import du model
 
-    //import de la vue 
-    include './view/view_show_all_.php';
+
     //import de la vue du menu
     include './view/view_menu_connecte.php';
+
+        //import de la vue 
+        include './view/view_menu.html';
     //import du footer 
     include './view/view_footer.html';
 ?>
