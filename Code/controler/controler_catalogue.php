f2eaac460b84096f50366cd90cb4def89db947ea
talique <ropars.k@gmail.com>
2022-05-16 15:32:11 +0200

    le merging de merde

--- Code/controler/controler_catalogue.php
+++ Code/controler/controler_catalogue.php
@@ -1,12 +1,5 @@
 <?php
-<<<<<<< HEAD
 include './view/view_menu_connecte.php';
 include './view/view_menu.html';
 include './view/view_footer.html';
-=======
-include '../view/view_menu_connecte.php';
-
-include '../view/view_menu.html';
-include '../view/view_footer.html';
->>>>>>> 9a066ae7bda31c50409f04cceccb8ff8c2a11be4
 ?> 
