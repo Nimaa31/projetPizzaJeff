d3c9aa7d61ca50b99c59737d95f8b1c74dd0c682
MrTruite <tomtutti@hotmail.fr>
2022-05-17 15:08:27 +0200

    add model user

--- Code/model/model_user.php
+++ Code/model/model_user.php
@@ -1,3 +1,48 @@
 <?php
- 
+    class Users{
+        private $id_compte;
+        private $mail_compte;
+        private $tel_compte;
+        private $adresse_compte;
+        private $id_droit;
+        // CONSTRUCTEUR
+        public function __construct($id, $mail, $tel, $adresse)
+        {
+            $this -> id_compte = $id;
+            $this -> mail_compte = $mail;
+            $this -> adresse_compte = $adresse;
+        }
+        // GETTER
+        public function getIdCompte():int{
+            return $this -> id_compte;
+        }
+        public function getMailCompte():string{
+            return $this -> mail_compte;
+        }
+        public function getTelCompte():string{
+            return $this -> tel_compte;
+        }
+        public function getAdresseCompte():string{
+            return $this -> adresse_compte;
+        }
+
+        // SETTER
+        public function setIdCompte($id):void{
+            $this -> id_compte = $id;
+        }
+        public function setMailCompte($mail):void{
+            $this -> mail_compte = $mail;
+        }
+        public function setTelCompte($tel):void{
+            $this -> tel_compte = $tel;
+        }
+        public function setAdresseCompte($adresse):void{
+            $this -> adresse_compte = $adresse;
+        }
+
+        public getUserByMail($bdd){
+            
+        }
+
+    }
 ?>
